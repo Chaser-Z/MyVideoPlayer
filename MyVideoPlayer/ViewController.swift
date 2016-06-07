d984d0a9a6ed1450d1a3d4fa998f803a4c08e086
yeeaoo <yeeaoo@yeeaoodeMacBook-Pro.local>
2016-06-07 09:42:28 +0800

    Initial Commit

---
+++ MyVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVideoPlayer
+//
+//  Created by yeeaoo on 16/6/7.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
